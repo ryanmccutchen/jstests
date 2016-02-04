91ef8b9ed0252e1a39428fa71bc5eead6aae7793
Ryan McCutchen <ryan.mccutchen@gmail.com>
2016-02-03 16:50:20 -0800

    new folder

--- Web01/js/skunk.js
+++ Web01/js/skunk.js
@@ -2,16 +2,25 @@
 		var self = this;
 		self.teams = ko.observableArray([]);
 		self.teamToAdd = ko.observable("");
-		self.selectedTeams = ko.observableArray();
+		self.selectedTeams = ko.observableArray([]);
 		self.addTeam = function () {
 			if(self.teamToAdd() != ""){
-				self.teams.push({favTeam: self.teamToAdd()});
+				self.teams.push({
+						favTeam: self.teamToAdd(), selected: ko.observable(false)
+					}
+				);
 				self.teamToAdd("");
 			}
 		};
 		self.removeTeams = function () {
-			self.teams.removeAll("self.selectedTeams()");
-			self.selectedTeams.removeAll();
+			ko.utils.arrayForEach(self.teams(), function(team){
+
+				if(team && team.selected() == true){
+					self.selectedTeams.push(team);
+				}
+			});
+			self.teams.removeAll(self.selectedTeams());
+			self.selectedTeams([]);
 		};
 	}
 	ko.applyBindings(new viewModel());	
